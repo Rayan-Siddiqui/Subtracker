8610547f019e20b565029ba49ea84dc6e312d626
Rayan-Siddiqui <rmsiddiqui115@gmail.com>
2026-04-06 12:32:22 -0400

    Connected MongoDB Atlas
    Created MongoDB cluster and connected to code.

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,24 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+// middleware
 app.use(cors());
 app.use(express.json());
+// database connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
+// routes
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server is working' });
 });
 
+//start server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
